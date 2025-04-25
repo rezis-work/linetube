43220498450ef1b15500535de14799a4186a5c29
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-04-25 10:23:01 +0400

    implement main layout

---
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -0,0 +1,31 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+import { SearchInput } from "./search-input";
+import AuthButton from "@/modules/auth/ui/components/auth-button";
+
+export const HomeNavbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+      <div className="flex items-center gap-4 w-full">
+        {/* Menu and Logo */}
+        <div className="flex items-center flex-shrink-0">
+          <SidebarTrigger />
+          <Link href={"/"}>
+            <Image src={"/logo.png"} alt="logo" width={150} height={150} />
+          </Link>
+        </div>
+
+        {/* Search Bar */}
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+          <SearchInput />
+        </div>
+
+        {/* Auth button */}
+        <div className="flex flex-shrink-0 items-center gap-4">
+          <AuthButton />
+        </div>
+      </div>
+    </nav>
+  );
+};
